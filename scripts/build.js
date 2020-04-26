f2777c918553edf0893973826491bb20ce42e8df
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2020-04-26 14:31:37 +0200

    chore: update build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,5 @@
-// Require Node.JS Dependencies
-const {
-    writeFile,
-    access
-} = require("fs").promises;
-const { constants: { R_OK } } = require("fs");
+// Require Node.js Dependencies
+const { constants: { R_OK }, promises: { writeFile, access } } = require("fs");
 const { join } = require("path");
 
 // Require Third-party Dependencies!
@@ -37,42 +33,33 @@ async function initializeConfiguration() {
 /** @type {rethinkdb.Connection} */
 let conn;
 
-/**
- * @async
- * @function build
- * @desc Build main entry
- * @returns {Promise<void>}
- */
-async function build() {
-    // Initialize configuration
-    const config = await initializeConfiguration();
+async function main() {
+    try {
+        const config = await initializeConfiguration();
 
-    // Connect to RethinkDB
-    conn = await rethinkdb.connect(config.database);
+        conn = await rethinkdb.connect(config.database);
 
-    // Create mordor database if not exist!
-    const currDBList = new Set(await rethinkdb.dbList().run(conn));
-    if (!currDBList.has("mordor")) {
-        const { dbs_created: dbCreated } = await rethinkdb.dbCreate("mordor").run(conn);
-        if (dbCreated !== 1) {
-            throw new Error("Failed to create Mordor database!");
+        // Create the mordor database if she doesn't exist!
+        const currDBList = new Set(await rethinkdb.dbList().run(conn));
+        if (!currDBList.has("mordor")) {
+            const { dbs_created: dbCreated } = await rethinkdb.dbCreate("mordor").run(conn);
+            if (dbCreated !== 1) {
+                throw new Error("Failed to create Mordor database!");
+            }
+        }
+
+        // Create require table(s) on mordor DB
+        const currTablesList = new Set(await rethinkdb.db("mordor").tableList().run(conn));
+        await Promise.all(
+            ["users"]
+                .filter((tableName) => !currTablesList.has(tableName))
+                .map((tableName) => rethinkdb.db("mordor").tableCreate(tableName).run(conn))
+        );
+    }
+    finally {
+        if (conn) {
+            await conn.close();
         }
     }
-
-    // Create require table(s) on mordor DB
-    const currTablesList = new Set(await rethinkdb.db("mordor").tableList().run(conn));
-    await Promise.all(
-        ["users"]
-            .filter((tableName) => !currTablesList.has(tableName))
-            .map((tableName) => rethinkdb.db("mordor").tableCreate(tableName).run(conn))
-    );
-
-    // Close DB connection
-    await conn.close();
 }
-build().catch((err) => {
-    console.error(err);
-    if (conn) {
-        conn.close();
-    }
-});
+main().catch(console.error);
